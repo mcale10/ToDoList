431885f9345fc013665fa264b97d93cffe06942e
Marko <marko.caleta@gmail.com>
2022-06-13 14:48:12 +0200

    added date js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 let PORT = process.env.port || 3000;
 
@@ -16,14 +17,7 @@ app.use(express.static("public")); //using public folder
 
 app.get("/", function(req, res) {
 
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleString("en-HR", options);
+    const day = date.getDate();
 
     res.render("list", { listTitle: day, newListItems: items });
 });
@@ -32,7 +26,7 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if (req.body.list === "Work") {
         workItems.push(item);
